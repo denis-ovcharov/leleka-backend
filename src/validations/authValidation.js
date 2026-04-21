000466ed48269cea6ffa2a11ba8d2f22536d57cb
Denys Ovcharov <denisovcharov88@gmail.com>
2026-04-22 00:20:12 +0200

    update

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -9,3 +9,23 @@ export const registerUserSchema = {
     dueDate: Joi.date().iso().allow(null),
   }),
 };
+
+export const loginUserSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+};
+
+export const requestResetEmailSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+  }),
+};
+
+export const resetPasswordSchema = {
+  [Segments.BODY]: Joi.object({
+    password: Joi.string().min(8).required(),
+    token: Joi.string().required(),
+  }),
+};
